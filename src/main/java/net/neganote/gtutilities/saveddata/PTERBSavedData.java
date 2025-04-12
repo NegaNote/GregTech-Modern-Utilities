f1eee4cd20d45afcdf15246b06369284f70de350
NegaNote <neganote43@gmail.com>
2025-04-12 00:10:06 -0400

    implement saving the PTERBSavedData

--- src/main/java/net/neganote/gtutilities/saveddata/PTERBSavedData.java
+++ src/main/java/net/neganote/gtutilities/saveddata/PTERBSavedData.java
@@ -3,6 +3,7 @@ package net.neganote.gtutilities.saveddata;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
@@ -39,18 +40,7 @@ public class PTERBSavedData extends SavedData {
             CompoundTag freqCompoundTag = (CompoundTag) t1;
             int freq = freqCompoundTag.getInt("frequency");
             var inputs = freqCompoundTag.getList("inputs", CompoundTag.TAG_COMPOUND);
-            List<Pair<ResourceLocation, BlockPos>> energyInputPairs = new ArrayList<>();
-            for (Tag t2 : inputs) {
-                CompoundTag energyInputTag = (CompoundTag) t2;
-                String dimensionNamespace = energyInputTag.getString("dimension_namespace");
-                String dimensionPath = energyInputTag.getString("dimension_path");
-                ResourceLocation dimension = new ResourceLocation(dimensionNamespace, dimensionPath);
-                int x = energyInputTag.getInt("block_pos_x");
-                int y = energyInputTag.getInt("block_pos_y");
-                int z = energyInputTag.getInt("block_pos_z");
-                BlockPos pos = new BlockPos(x, y, z);
-                energyInputPairs.add(new Pair<>(dimension, pos));
-            }
+            List<Pair<ResourceLocation, BlockPos>> energyInputPairs = getEnergyPairs(inputs);
             energyInputs.put(freq, energyInputPairs);
         }
 
@@ -59,20 +49,25 @@ public class PTERBSavedData extends SavedData {
             CompoundTag freqCompoundTag = (CompoundTag) t1;
             int freq = freqCompoundTag.getInt("frequency");
             var outputs = freqCompoundTag.getList("outputs", CompoundTag.TAG_COMPOUND);
-            List<Pair<ResourceLocation, BlockPos>> energyOutputPairs = new ArrayList<>();
-            for (Tag t2 : outputs) {
-                CompoundTag energyOutputTag = (CompoundTag) t2;
-                String dimensionNamespace = energyOutputTag.getString("dimension_namespace");
-                String dimensionPath = energyOutputTag.getString("dimension_path");
-                ResourceLocation dimension = new ResourceLocation(dimensionNamespace, dimensionPath);
-                int x = energyOutputTag.getInt("block_pos_x");
-                int y = energyOutputTag.getInt("block_pos_y");
-                int z = energyOutputTag.getInt("block_pos_z");
-                BlockPos pos = new BlockPos(x, y, z);
-                energyOutputPairs.add(new Pair<>(dimension, pos));
-            }
+            List<Pair<ResourceLocation, BlockPos>> energyOutputPairs = getEnergyPairs(outputs);
             energyOutputs.put(freq, energyOutputPairs);
         }
+    }
+
+    private static @NotNull List<Pair<ResourceLocation, BlockPos>> getEnergyPairs(ListTag tags) {
+        List<Pair<ResourceLocation, BlockPos>> energyPairs = new ArrayList<>();
+        for (Tag t2 : tags) {
+            CompoundTag energyTag = (CompoundTag) t2;
+            String dimensionNamespace = energyTag.getString("dimension_namespace");
+            String dimensionPath = energyTag.getString("dimension_path");
+            ResourceLocation dimension = new ResourceLocation(dimensionNamespace, dimensionPath);
+            int x = energyTag.getInt("block_pos_x");
+            int y = energyTag.getInt("block_pos_y");
+            int z = energyTag.getInt("block_pos_z");
+            BlockPos pos = new BlockPos(x, y, z);
+            energyPairs.add(new Pair<>(dimension, pos));
+        }
+        return energyPairs;
     }
 
     public static PTERBSavedData getOrCreate(ServerLevel serverLevel) {
@@ -82,6 +77,51 @@ public class PTERBSavedData extends SavedData {
 
     @Override
     public @NotNull CompoundTag save(@NotNull CompoundTag nbt) {
+        CompoundTag energyInputsTag = new CompoundTag();
+        CompoundTag energyOutputsTag = new CompoundTag();
+
+        ListTag energyInputFrequencies = new ListTag();
+        for (Integer freq : energyInputs.keySet()) {
+            CompoundTag freqCompoundTag = new CompoundTag();
+            freqCompoundTag.putInt("frequency", freq);
+
+            List<Pair<ResourceLocation, BlockPos>> energyInputPairs = energyInputs.get(freq);
+            ListTag inputs = getEnergyTags(energyInputPairs);
+            freqCompoundTag.put("inputs", inputs);
+            energyInputFrequencies.add(freqCompoundTag);
+        }
+        energyInputsTag.put(ENERGY_INPUT_FREQUENCIES, energyInputFrequencies);
+        nbt.put("energy_inputs", energyInputsTag);
+
+        ListTag energyOutputFrequencies = new ListTag();
+        for (Integer freq : energyInputs.keySet()) {
+            CompoundTag freqCompoundTag = new CompoundTag();
+            freqCompoundTag.putInt("frequency", freq);
+
+            List<Pair<ResourceLocation, BlockPos>> energyInputPairs = energyInputs.get(freq);
+            ListTag inputs = getEnergyTags(energyInputPairs);
+            freqCompoundTag.put("outputs", inputs);
+            energyOutputFrequencies.add(freqCompoundTag);
+        }
+        energyOutputsTag.put(ENERGY_OUTPUT_FREQUENCIES, energyOutputFrequencies);
+        nbt.put("energy_outputs", energyOutputsTag);
+
         return nbt;
+    }
+
+    private static @NotNull ListTag getEnergyTags(List<Pair<ResourceLocation, BlockPos>> pairs) {
+        ListTag tags = new ListTag();
+        for (Pair<ResourceLocation, BlockPos> pair : pairs) {
+            CompoundTag energyTag = new CompoundTag();
+            ResourceLocation dimension = pair.getFirst();
+            energyTag.putString("dimension_namespace", dimension.getNamespace());
+            energyTag.putString("dimension_path", dimension.getPath());
+            BlockPos pos = pair.getSecond();
+            energyTag.putInt("block_pos_x", pos.getX());
+            energyTag.putInt("block_pos_y", pos.getY());
+            energyTag.putInt("block_pos_z", pos.getZ());
+            tags.add(energyTag);
+        }
+        return tags;
     }
 }
