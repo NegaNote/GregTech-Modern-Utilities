fb204243f2e86c0277052215d74ed8fe826f8bee
NegaNote <neganote43@gmail.com>
2025-04-15 23:01:12 -0400

    add PTERB recipe

--- src/main/java/net/neganote/gtutilities/recipe/UtilRecipes.java
+++ src/main/java/net/neganote/gtutilities/recipe/UtilRecipes.java
@@ -1,16 +1,23 @@
 package net.neganote.gtutilities.recipe;
 
 import com.gregtechceu.gtceu.api.GTValues;
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.common.data.GTItems;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
+import com.gregtechceu.gtceu.common.data.machines.GTMultiMachines;
 import com.gregtechceu.gtceu.config.ConfigHolder;
+import com.gregtechceu.gtceu.data.recipe.CustomTags;
 
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.world.item.ItemStack;
+import net.neganote.gtutilities.common.machine.UtilMachines;
 import net.neganote.gtutilities.config.UtilConfig;
 
 import java.util.function.Consumer;
 
+import static com.gregtechceu.gtceu.common.data.GTBlocks.LASER_PIPES;
 import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.ASSEMBLER_RECIPES;
+import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.ASSEMBLY_LINE_RECIPES;
 import static com.gregtechceu.gtceu.data.recipe.CraftingComponent.*;
 import static net.neganote.gtutilities.common.item.UtilItems.OMNIBREAKER;
 import static net.neganote.gtutilities.common.machine.UtilMachines.ENERGY_CONVERTER_64A;
@@ -25,6 +32,26 @@ public class UtilRecipes {
 
         if (UtilConfig.INSTANCE.features.omnibreakerEnabled) {
             registerOmnitoolRecipe(provider);
+        }
+
+        if (UtilConfig.INSTANCE.features.pterbEnabled) {
+            ASSEMBLY_LINE_RECIPES.recipeBuilder("pterb")
+                    .inputItems(GTMultiMachines.ACTIVE_TRANSFORMER)
+                    .inputItems(TagPrefix.plate, GTMaterials.Neutronium, 32)
+                    .inputItems(SENSOR.getIngredient(GTValues.UV), 8)
+                    .inputItems(EMITTER.getIngredient(GTValues.UV), 8)
+                    .inputItems(FIELD_GENERATOR.getIngredient(GTValues.UV), 4)
+                    .inputItems(CustomTags.UHV_CIRCUITS, 2)
+                    .inputItems(TagPrefix.pipeLargeFluid, GTMaterials.Neutronium, 4)
+                    .inputItems(CABLE_QUAD.getIngredient(GTValues.UV), 8)
+                    .inputItems(LASER_PIPES[0], 8)
+                    .inputFluids(GTMaterials.SolderingAlloy.getFluid(144 * 32))
+                    .EUt(1_600_000L)
+                    .duration(1200)
+                    .outputItems(UtilMachines.PTERB_MACHINE)
+                    .stationResearch(b -> b
+                            .researchStack(GTMultiMachines.ACTIVE_TRANSFORMER.asStack()).CWUt(16))
+                    .save(provider);
         }
     }
 
