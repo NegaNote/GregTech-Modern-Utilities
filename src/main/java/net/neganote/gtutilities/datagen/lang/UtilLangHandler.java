89e370b77deb6f0742698878b4ec16058083e1db
Tyler <3821892+kolatra@users.noreply.github.com>
2025-06-22 16:34:35 -0600

    Complete functionality, information pending

--- src/main/java/net/neganote/gtutilities/datagen/lang/UtilLangHandler.java
+++ src/main/java/net/neganote/gtutilities/datagen/lang/UtilLangHandler.java
@@ -14,6 +14,8 @@ import java.util.Set;
 public class UtilLangHandler {
 
     public static void init(RegistrateLangProvider provider) {
+        provider.add("tooltip.omnibreaker.swappable_tools", "Also contains multiple tools in one!");
+        provider.add("tooltip.omnibreaker.tool_mode", "Current mode: %s");
         provider.add("tooltip.omnibreaker.can_break_anything", "The Omni-breaker can insta-mine ANYTHING!");
         provider.add("tooltip.omnibreaker.charge_status", "Energy: %s EU / %s EU");
         provider.add("tooltip.omnibreaker.right_click_function", "Break individual blocks with right-click!");
